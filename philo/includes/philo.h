4bb361e74cb7611ae4837aafef0ef1cf79f6ce18
Marc Lindenmaier <mlindenm@2-F-8.42heilbronn.de>
2023-06-11 22:51:35 +0200

    started doing sth

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,14 +6,16 @@
 /*   By: mlindenm <mlindenm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 18:15:16 by mlindenm          #+#    #+#             */
-/*   Updated: 2023/05/31 18:19:43 by mlindenm         ###   ########.fr       */
+/*   Updated: 2023/06/10 13:13:35 by mlindenm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
+# include <pthread.h>
 # include <unistd.h> //NULL; write();
+# include <stdio.h>
 
 // error.c
 void	error(char *reason);
--- philo/sources/main.c
+++ philo/sources/main.c
@@ -6,14 +6,32 @@
 /*   By: mlindenm <mlindenm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 18:14:50 by mlindenm          #+#    #+#             */
-/*   Updated: 2023/05/31 18:19:12 by mlindenm         ###   ########.fr       */
+/*   Updated: 2023/06/10 13:31:15 by mlindenm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
+void*	routine()
+{
+	printf("Test\n");
+	usleep(1000000);
+	printf("Ending\n");
+}
+
 int	main(void)
 {
-	write(1,"philo\n", 6);
+	pthread_t	t1;
+	pthread_t	t2;
+
+	if (pthread_create(&t1, NULL, &routine, NULL) != 0)
+		return (0);
+	if (pthread_create(&t2, NULL, &routine, NULL) != 0)
+		return (0);
+	if (pthread_join(t1, NULL) != 0)
+		return (0);
+	if (pthread_join(t2, NULL) != 0)
+		return (0);
+	// write(1,"philo\n", 6);
 	return (0);
 }
